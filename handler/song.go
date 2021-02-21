65502e730e9fb35f0a775413c6ab8fd9fc120996
Arman Aminian <arman.aminian78@gmail.com>
2021-02-21 19:16:16 +0330

    save to file func added add song updated

--- handler/song.go
+++ handler/song.go
@@ -9,8 +9,8 @@ import (
 	"github.com/arman-aminian/type-your-song/utils"
 	"github.com/labstack/echo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io/ioutil"
 	"net/http"
-	"os"
 	"strconv"
 )
 
@@ -34,11 +34,13 @@ func (h *Handler) AddSong(c echo.Context) error {
 
 	var s model.Song
 	s.Name = c.FormValue("name")
+	// todo cover should be file
 	s.Cover = c.FormValue("cover")
 	s.Genre = c.FormValue("genre")
 	s.Artist, err = primitive.ObjectIDFromHex(c.FormValue("artist"))
 	if err != nil {
-		return c.JSON(http.StatusNotFound, utils.NewError(errors.New("artist not found")))
+
+		//return c.JSON(http.StatusNotFound, utils.NewError(errors.New("artist not found")))
 	}
 	s.Duration, _ = strconv.Atoi(c.FormValue("duration"))
 	s.PassedUsers = &[]primitive.ObjectID{}
@@ -47,10 +49,12 @@ func (h *Handler) AddSong(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusUnprocessableEntity, utils.NewError(errors.New("invalid srt file")))
 	}
-	f, err := os.Open(lyrics.Filename)
+	fh, err := lyrics.Open()
 	if err != nil {
+		fmt.Println("err : ", err)
 		return c.JSON(http.StatusInternalServerError, utils.NewError(errors.New("error on parse subtitle")))
 	}
+	f, err := ioutil.ReadAll(fh)
 	srt, err := ggosub.ParseByFile(f)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, utils.NewError(errors.New("error on parse subtitle")))
--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,31 @@
 package utils
 
+import (
+	"io"
+	"mime/multipart"
+	"os"
+)
+
 const (
 	BaseUrl = "127.0.0.1:8080"
 )
+
+func SaveToFiles(f multipart.FileHeader, path string) (string, error) {
+	src, err := f.Open()
+	if err != nil {
+		return "", err
+	}
+	defer src.Close()
+
+	mediaPath := path + f.Filename
+	dst, err := os.Create(mediaPath)
+	if err != nil {
+		return "", err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return "", err
+	}
+	return mediaPath, nil
+}
